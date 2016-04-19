1afe8fe337b95e3933688a1f3d861fe1165a9a09
z448 <zed448@icloud.com>
2016-04-19 21:25:33 +0200

    +serialize to json & dumper in ~/.alios.json & ~/.alios.dmp

--- alios.pl
+++ alios.pl
@@ -5,10 +5,16 @@ use File::Find;
 use Encode;
 use Data::Dumper;
 use Term::ANSIColor;
+use Getopt::Std;
+use JSON qw< encode_json >;
 use open qw< :encoding(UTF-8) >;
 
+my $option = {};
+my ($storage) = ();
+my $dump = "$ENV{HOME}/.alios.dmp";
 my @search_base = ("$ENV{HOME}/Containers/Data/Application","$ENV{HOME}/Containers/Shared/AppGroup");
 my $app = {};
+getopts('d:m:s', $option);
 
 find( sub{ 
         my $plist_path = "$File::Find::dir/$_";
@@ -24,9 +30,20 @@ for(keys %$app){
     print colored(['green'], 'uuid: ') . $app->{$_} . "\n";
 }
 
+my $dumper = Data::Dumper->Dump([$app]);
+open(my $fh,">",$dump);
+print $dumper;
+print $fh $dumper;
+close $fh; undef $fh;
+
+open($fh,"<","$ENV{HOME}/.alios.dmp");
+while(<$fh>){ $storage .= $_ }
+close $fh; undef $fh;
 
 
+#print Dumper($conf);
+eval $storage;
+print $VAR1->{"com.opera.Coast.plist"};
 
-
-
-
+my $json = encode_json $app;
+#print $json;
