23de6df27a855c5d22bc32cfd9220534124a8811
z448 <zed448@icloud.com>
2016-04-20 12:15:06 +0200

    -f option searches found app IDs

--- alios.pl
+++ alios.pl
@@ -11,8 +11,7 @@ use JSON qw< encode_json >;
 use open qw< :encoding(UTF-8) >;
 
 my $option = {};
-my $app = {};
-my (@search_base, $storable, $json) = ();
+my (@app, @search_base, $storable, $json) = ();
 my @search_base = ("$ENV{HOME}/Containers/Data/Application","$ENV{HOME}/Containers/Shared/AppGroup");
 $storable = "$ENV{HOME}/.alios.dat";
 $json = "$ENV{HOME}/.alios.json";
@@ -24,20 +23,16 @@ find( sub{
         if($plist_path =~ /Library\/Preferences\/.*\.plist/){
             my $match = "$File::Find::dir/$_";
             $match =~ s/(.*)(\/App.*?\/)(.*?)(\/Library\/Preferences\/)(.*)(\.plist)/$1$2$3$4$5/;
-            $app->{apnr} = $i;
-            $app->{apid} = $5;
-            $app->{uuid} = $3;
+            $app{apnr} = $i;
+            $app{apid} = $5;
+            $app{uuid} = $3;
+            push @app,{%app};
             $i++;
 }}, @search_base );
 
-#for(keys %$app){
-#    print colored(['green'], 'appid: ') . $_ . "\n";
-#    print colored(['green'], 'uuid : ') . $app->{$_} . "\n";
-#}
-
 # --json
 open(my $fh,">",$json);
-$json = encode_json $app;
+$json = encode_json \@app;
 print $fh $json;
 close $fh; undef $fh;
 
@@ -46,19 +41,24 @@ close $fh; undef $fh;
 #print "Error writing to file: $@" if $@;
 #$storable = retrieve($storable);
 
+# --search appids
 my $map = sub {
-    my $apid = shift;
-    my @match = grep { $app->{apid} eq $apid } %$app;
+    my @match = ();
+    my $filter = qr/$option->{f}/;
+    my @filtered = grep { $_->{"apid"} =~ /$filter/ } @app;
+    for(@filtered){ 
+        my $match = $_->{apnr} . ' >> ' . $_->{apid} . ' >> ' . $_->{uuid};
+        push @match, $match;
+    };  return \@match;
+};      
 
-    for(@match){ say $_ }
-};
 
-$map->($option->{f});
-__DATA__
-my @all_matches = grep { $fruit{$_} eq 'yellow' } keys %fruit;
-print("$_ ") foreach @matching_keys;
+say @{$map->()};
 
-my ($any_match) = grep { $fruit{$_} eq 'yellow' } keys %fruit;
 
+
+
+
+        
 
 
